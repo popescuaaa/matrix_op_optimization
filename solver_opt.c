2561cbbc1f28ce6fd23027b4ce66c5240851173a
Andrei Popescu <andreipopescu@192-168-0-104.rdsnet.ro>
2020-04-15 10:55:19 +0300

    Added printed directives for dedub

--- solver_opt.c
+++ solver_opt.c
@@ -11,11 +11,43 @@
 double* my_solver(int N, double *A, double* B) 
 {
 	/* Computing the main parameters used for computation */ 
-	double* At = transpose(N, A);
-	double* A2 = multiply_opt(N, A, A);
-	double* T1 = multiply_opt(N, B, At);
-	double* T2 = multiply_opt(N, A2, B);
+	size_t li;
+	size_t ci;
+	for (li = 0; li < N; li++) {
+		for (ci = 0; ci < N; ci++) {
+			printf("%lf ", A[li * N + ci]);
+		}
+		printf("\n");
+	}
 
+	double* At = transpose(N, A);
+	for (li = 0; li < N; li++) {
+		for (ci = 0; ci < N; ci++) {
+			printf("%lf ", At[li * N + ci]);
+		}
+		printf("\n");
+	}
+	double* A2 = multiply_opt(N, A, A);
+	for (li = 0; li < N; li++) {
+		for (ci = 0; ci < N; ci++) {
+			printf("%lf ", A2[li * N + ci]);
+		}
+		printf("\n");
+	}
+	double* T1 = multiply_opt(N, B, At);
+	for (li = 0; li < N; li++) {
+		for (ci = 0; ci < N; ci++) {
+			printf("%lf ", T1[li * N + ci]);
+		}
+		printf("\n");
+	}
+	double* T2 = multiply_opt(N, A2, B);
+	for (li = 0; li < N; li++) {
+		for (ci = 0; ci < N; ci++) {
+			printf("%lf ", T2[li * N + ci]);
+		}
+		printf("\n");
+	}
 	double* R = matrix_add(N, T1, T2);
 	
 	free(At);
