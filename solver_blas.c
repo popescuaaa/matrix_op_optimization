d4dd43ff48dc85f40efd688d96c1694247ea569f
Andrei Popescu <andreipopescu@192-168-0-104.rdsnet.ro>
2020-04-18 15:59:01 +0300

    Added proper memory allocation for blas method

--- solver_blas.c
+++ solver_blas.c
@@ -43,19 +43,19 @@ double* my_solver(int N, double *A, double *B) {
 	size_t li;
 	size_t ci;
 	
-	posix_memalign((void**) &B_At, 64, N * N * sizeof(double));
+	B_At = (double *) calloc(N * N, sizeof(double)));
 	if (B_At == NULL)
 		exit(BAD_ALLOC);
 
-	posix_memalign((void**) &A2, 64, N * N * sizeof(double));
+	A2 = (double *) calloc(N * N, sizeof(double)));
 	if (A2 == NULL)
 		exit(BAD_ALLOC);
 
-	posix_memalign((void**) &A2_B, 64, N * N * sizeof(double));
+	A2_B = (double *) calloc(N * N, sizeof(double)));
 	if (A2_B == NULL)
 		exit(BAD_ALLOC);
 
-	posix_memalign((void**) &R, 64, N * N * sizeof(double));
+	R = (double *) calloc(N * N, sizeof(double)));
 	if (R == NULL)
 		exit(BAD_ALLOC);
 
