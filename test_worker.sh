5d5a77113b75b071f31f537d42fa7d302ee27e9e
Andrei Popescu <andreipopescu@Andreis-MacBook-Pro.local>
2020-04-18 23:26:03 +0300

    Made small modifications for optimal <<m>> method - testing sesion #1

--- test_worker.sh
+++ test_worker.sh
@@ -17,64 +17,59 @@ chmod 777 ./tema2_blas
 chmod 777 ./tema2_neopt
 chmod 777 ./tema2_opt_m
 
-echo "Testing blas API method..."
-# Test blas flavour
-echo "--------------------------------" >> logile
-./tema2_blas input &>> logile
-echo "--------------------------------" >> logile
-#------------------- Crectness -------------------------
-./compare out1 /export/asc/tema2/out1 0.01
-./compare out2 /export/asc/tema2/out2 0.01
-./compare out3 /export/asc/tema2/out3 0.01
+for (( c=1; c<=10; c++ ))
+do  
+    echo "Testing blas API method..."
+    # Test blas flavour
+    ./tema2_blas input &>> logile
+    #------------------- Crectness -------------------------
+    ./compare out1 /export/asc/tema2/out1 0.01
+    ./compare out2 /export/asc/tema2/out2 0.01
+    ./compare out3 /export/asc/tema2/out3 0.01
 
-# ------------- Execution for NEOPT --------------------
-#
-echo "Testing neoptimal method..."
-# Test neopt flavour
-echo "--------------------------------" >> logile
-./tema2_neopt input &>> logile
-echo "--------------------------------" >> logile
+    # ------------- Execution for NEOPT --------------------
+    #
+    echo "Testing neoptimal method..."
+    # Test neopt flavour
+    ./tema2_neopt input &>> logile
 
-#------------------- Crectness -------------------------
-./compare out1 /export/asc/tema2/out1 0.01
-./compare out2 /export/asc/tema2/out2 0.01
-./compare out3 /export/asc/tema2/out3 0.01
+    #------------------- Crectness -------------------------
+    ./compare out1 /export/asc/tema2/out1 0.01
+    ./compare out2 /export/asc/tema2/out2 0.01
+    ./compare out3 /export/asc/tema2/out3 0.01
 
-# ------------- Execution for OPT_M --------------------
-#
-echo "Testing optimal <<m>> method..."
-# Test opt_m flavour
-echo "--------------------------------" >> logile
-./tema2_opt_m input &>> logile
-echo "--------------------------------" >> logile
+    # ------------- Execution for OPT_M --------------------
+    #
+    echo "Testing optimal <<m>> method..."
+    # Test opt_m flavour
+    ./tema2_opt_m input &>> logile
 
-#------------------- Crectness -------------------------
-./compare out1 /export/asc/tema2/out1 0.01
-./compare out2 /export/asc/tema2/out2 0.01
-./compare out3 /export/asc/tema2/out3 0.01
+    #------------------- Crectness -------------------------
+    ./compare out1 /export/asc/tema2/out1 0.01
+    ./compare out2 /export/asc/tema2/out2 0.01
+    ./compare out3 /export/asc/tema2/out3 0.01
 
-# ------------- Execution for OPT_F --------------------
-#
-echo "Testing optimal <<f>> method..."
-# Test opt_m flavour
-echo "--------------------------------" >> logfile
-./tema2_opt_f input &>> logile 
-echo "--------------------------------" >> logfile
+    # ------------- Execution for OPT_F --------------------
+    #
+    echo "Testing optimal <<f>> method..."
+    # Test opt_m flavour
+    ./tema2_opt_f input &>> logile 
 
-#------------------- Crectness -------------------------
-./compare out1 /export/asc/tema2/out1 0.01
-./compare out2 /export/asc/tema2/out2 0.01
-./compare out3 /export/asc/tema2/out3 0.01
+    #------------------- Crectness -------------------------
+    ./compare out1 /export/asc/tema2/out1 0.01
+    ./compare out2 /export/asc/tema2/out2 0.01
+    ./compare out3 /export/asc/tema2/out3 0.01
 
-# ------------- Execution for OPT_F_EXTRA --------------
-#
-echo "Testing optimal <<f_extra>> method..."
-# Test opt_m flavour
-echo "--------------------------------" >> logfile
-./tema2_opt_f_extra input &>> logile
-echo "--------------------------------" >> logfile
+    # ------------- Execution for OPT_F_EXTRA --------------
+    #
+    echo "Testing optimal <<f_extra>> method..."
+    # Test opt_m flavour
+    ./tema2_opt_f_extra input &>> logile
 
-#------------------- Crectness -------------------------
-./compare out1 /export/asc/tema2/out1 0.01
-./compare out2 /export/asc/tema2/out2 0.01
-./compare out3 /export/asc/tema2/out3 0.01
+    #------------------- Crectness -------------------------
+    ./compare out1 /export/asc/tema2/out1 0.01
+    ./compare out2 /export/asc/tema2/out2 0.01
+    ./compare out3 /export/asc/tema2/out3 0.01
+
+done
+
