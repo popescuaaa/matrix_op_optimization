01b13c8a6d8947a8b3b156ae98da63db7117c459
Andrei Popescu <andreipopescu@192-168-0-104.rdsnet.ro>
2020-04-20 17:11:37 +0300

    Final optimization and testing steps <<3>>

--- test_worker.sh
+++ test_worker.sh
@@ -22,6 +22,7 @@ do
     echo "Testing blas API method..."
     # Test blas flavour
     ./tema2_blas input &>> logile
+    ./tema2_blas custom_input &>> custom_logile
     #------------------- Crectness -------------------------
     ./compare out1 /export/asc/tema2/out1 0.001
     ./compare out2 /export/asc/tema2/out2 0.001
@@ -32,6 +33,7 @@ do
     echo "Testing neoptimal method..."
     # Test neopt flavour
     ./tema2_neopt input &>> logile
+    ./tema2_neopt custom_input &>> custom_logile
 
     #------------------- Crectness -------------------------
     ./compare out1 /export/asc/tema2/out1 0.001
@@ -43,7 +45,7 @@ do
     echo "Testing optimal <<m>> method..."
     # Test opt_m flavour
     ./tema2_opt_m input &>> logile
-
+    ./tema2_opt_m custom_input &>> custom_logile
     #------------------- Crectness -------------------------
     ./compare out1 /export/asc/tema2/out1 0.001
     ./compare out2 /export/asc/tema2/out2 0.001
@@ -54,7 +56,7 @@ do
     echo "Testing optimal <<f>> method..."
     # Test opt_m flavour
     ./tema2_opt_f input &>> logile 
-
+    ./tema2_opt_f custom_input &>> custom_logile
     #------------------- Crectness -------------------------
     ./compare out1 /export/asc/tema2/out1 0.001
     ./compare out2 /export/asc/tema2/out2 0.001
@@ -65,7 +67,7 @@ do
     echo "Testing optimal <<f_extra>> method..."
     # Test opt_m flavour
     ./tema2_opt_f_extra input &>> logile
-
+    ./tema2_opt_f_extra custom_input &>> custom_logile
     #------------------- Crectness -------------------------
     ./compare out1 /export/asc/tema2/out1 0.001
     ./compare out2 /export/asc/tema2/out2 0.001
