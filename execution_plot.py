7d9b0bb69edd908a22012b706fadff8a0271038e
Andrei Popescu <andreipopescu@192-168-0-104.rdsnet.ro>
2020-04-20 16:59:05 +0300

    Final optimization and testing steps <<2>>

--- execution_plot.py
+++ execution_plot.py
@@ -1,20 +1,18 @@
-import matplotlib.pyplot as plt
-
 FILE_NAME = "logfile"
 TIME_COLUMN = 2
 NAME_COLUMN = 0
 
-# execution time values 
+# execution time values
 NEOPT = list()
 OPT_M = list()
 OPT_F = list()
 OPT_F_EXTRA = list()
 BLAS = list()
 
+DATA = [NEOPT, OPT_M, OPT_F, OPT_F_EXTRA, BLAS]
 
 def extract_data():
     file = open(FILE_NAME, "r")
-    
     for line in file:
         if "N=1200" in line:
             line_tokens = line.split(" ")
@@ -31,14 +29,16 @@ def extract_data():
                 OPT_F.append(time_value)
             if "opt_f_extra" in name:
                 OPT_F_EXTRA.append(time_value)
-            
+    file.close()
 
-def plot_data():
+def write_data():
     extract_data()
-    plt.plot(NEOPT, OPT_M)
-    plt.xlabel("time")
-    plt.show()
-
+    file = open("plot", "w")
+    for arr in DATA:
+        file.write('============================\n')
+        for elem in arr:
+            file.write('{}\n'.format(elem))     
+    file.close()
 
 if __name__ == "__main__":
-    plot_data()
+   write_data()
